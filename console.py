33bced8f5a2f75f58acfa857620c0f8192ad7e6a
saminstein <saminstein@yahoo.com>
2024-06-07 21:51:11 +0100

    .

--- console.py
+++ console.py
@@ -4,6 +4,7 @@ This module implements the cmd for the console
 """
 
 import cmd as c
+import os
 
 
 class HBNBCommand(c.Cmd):
@@ -11,10 +12,20 @@ class HBNBCommand(c.Cmd):
     """
     prompt = "(hbnb) "
 
+
     def do_prompt(self, line):
+        '''
+        '''
         self.prompt = line
 
     def do_EOF(self, line):
+        '''
+        '''
+        return True
+
+    def do_quit(self, line):
+        '''
+        '''
         return True
 
 if __name__ == '__main__':
