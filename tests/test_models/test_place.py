9f2580bbba233cf9cca3285aa8101616ef51653b
saminstein <saminstein@yahoo.com>
2024-08-14 17:58:48 +0100

    test_place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -59,4 +59,93 @@ class TestPlace(unittest.TestCase):
         self.assertEqual(self.place.name, '')
         self.place.name = 'London'
         self.assertEqual(self.place.name, 'London')
-    
+
+    def test_description(self):
+        '''
+        tests if description is an attr of Place & has the correct value
+        '''
+
+        self.assertTrue(hasattr(self.place, 'description'))
+        self.assertEqual(self.place.description, '')
+        self.place.description = 'Spacious'
+        self.assertEqual(self.place.description, 'Spacious')
+
+    def test_number_rooms(self):
+        '''
+        tests if number_rooms is an attr of Place & has the correct value
+        '''
+
+        self.assertTrue(hasattr(self.place, 'number_rooms'))
+        self.assertEqual(type(self.place.number_rooms), int)
+        self.assertEqual(self.place.number_rooms, 0)
+        self.place.number_rooms = 36
+        self.assertEqual(self.place.number_rooms, 36)
+
+    def test_number_bathrooms(self):
+        '''
+        tests if number_bathrooms is an attr of Place & has the correct value
+        '''
+
+        self.assertTrue(hasattr(self.place, 'number_bathrooms'))
+        self.assertIsInstance(self.place.number_bathrooms, int)
+        self.assertEqual(self.place.number_bathrooms, 0)
+        self.place.number_bathrooms = 1
+        self.assertEqual(self.place.number_bathrooms, 1)
+
+    def test_max_guest(self):
+        '''
+        tests if max_guest is an attr of Place & has the correct value
+        '''
+
+        self.assertTrue(hasattr(self.place, 'max_guest'))
+        self.assertIsInstance(self.place.max_guest, int)
+        self.assertEqual(self.place.max_guest, 0)
+        self.place.max_guest = 3
+        self.assertEqual(self.place.max_guest, 3)
+
+    def test_price_by_night(self):
+        '''
+        tests if price_by_night is an attr of Place & has the correct value
+        '''
+
+        self.assertTrue(hasattr(self.place, 'price_by_night'))
+        self.assertIsInstance(self.place.price_by_night, int)
+        self.assertEqual(self.place.price_by_night, 0)
+        self.place.price_by_night = 5
+        self.assertEqual(self.place.price_by_night, 5)
+
+    def test_latitude_attr(self):
+        '''
+        tests if latitude is an attr of Place & has the correct value
+        '''
+
+        self.assertTrue(hasattr(self.place, 'latitude'))
+        self.assertIsInstance(self.place.latitude, float)
+        self.assertEqual(self.place.price_by_night, 0.0)
+        self.place.latitude = 2.82
+        self.assertEqual(self.place.latitude, 2.82)
+
+    def test_longitude_attr(self):
+        '''
+        tests if longitude is an attr of Place& has the correct value
+        '''
+
+        self.assertTrue(hasattr(self.place, 'longitude'))
+        self.assertIsInstance(self.place.longitude, float)
+        self.assertEqual(self.place.longitude, 0.0)
+        self.place.longitude = 3.42
+        self.assertEqual(self.place.longitude, 3.42)
+
+    def test_amenity_ids(self):
+        '''
+        tests if amenity_ids is an attr of Place & has the correct value
+        '''
+
+        self.assertTrue(hasattr(self.place, 'amenity_ids'))
+        self.assertIsInstance(self.place.amenity_ids, list)
+        self.assertEqual(self.place.amenity_ids, [])
+        self.place.amenity_ids = ['wifi', 'hot water']
+        self.assertEqual(self.place.amenity_ids, ['wifi', 'hot water'])
+
+if __name__ == "__main__":
+    unittest.main()
